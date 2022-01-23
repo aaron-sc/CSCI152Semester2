1e070a90c88d6e02a7593e5d57a582949c769812
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-01-22 20:56:23 -0700

    Assignment 1

---
+++ Assignment 1/src/GreatCircle.java
@@ -0,0 +1,14 @@
+public class GreatCircle {
+    public static void main(String[] args) {
+        double r = 6371.0;
+        double x1 = Math.toRadians(Double.parseDouble(args[0]));
+        double y1 = Math.toRadians(Double.parseDouble(args[1]));
+        double x2 = Math.toRadians(Double.parseDouble(args[2]));
+        double y2 = Math.toRadians(Double.parseDouble(args[3]));
+        double op1 = (x2 - x1);
+        double op2 = (y2 - y1);
+        double internal = (Math.pow(Math.sin(op1 / 2), 2)) + (Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin(op2 / 2), 2));
+        double distance = ((2 * r) * Math.asin(Math.sqrt(internal)));
+        System.out.println(distance + " kilometers");
+    }
+}
