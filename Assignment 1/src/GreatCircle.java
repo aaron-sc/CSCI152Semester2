dba8c999ede0c710b6a529a8051b355dd0f7e4df
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-01-25 11:45:18 -0700

    complete

--- Assignment 1/src/GreatCircle.java
+++ Assignment 1/src/GreatCircle.java
@@ -9,6 +9,6 @@ public class GreatCircle {
         double op2 = (y2 - y1);
         double internal = (Math.pow(Math.sin(op1 / 2), 2)) + (Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin(op2 / 2), 2));
         double distance = ((2 * r) * Math.asin(Math.sqrt(internal)));
-        System.out.println(distance + " kilometers");
+        StdOut.println(distance + " kilometers");
     }
 }
--- Assignment 1/src/RightTriangle.java
+++ Assignment 1/src/RightTriangle.java
@@ -3,6 +3,10 @@ public class RightTriangle {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
-        StdOut.println(((a ^ 2) + (b ^ 2) == (c ^ 2)));
+        boolean pos = a > 0 && b > 0 && c > 0;
+        boolean t1 = (a * a + b * b == c * c);
+        boolean t2 = (a * a + c * c == b * b);
+        boolean t3 = (c * c + b * b == a * a);
+        StdOut.println((t1 || t2 || t3) && pos);
     }
 }
