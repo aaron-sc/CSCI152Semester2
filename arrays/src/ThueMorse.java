f73e7d6402b1debbbabf298364085aaf747f2935
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-02-08 10:58:44 -0700

    Worked on #1 & #2

---
+++ arrays/src/ThueMorse.java
@@ -0,0 +1,18 @@
+public class ThueMorse {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int[] choices = {0, 1};
+
+        for (int i = 0; i < n; i++) {
+            boolean state = false;
+
+            int[] r = new int[n];
+            
+            for (int j = 0; j < n; j++) {
+                r[j] = choices[i];
+            }
+        }
+
+
+    }
+}
