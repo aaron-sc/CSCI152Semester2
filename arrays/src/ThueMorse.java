a7862ae6b392ff8d1d05a14b89cbd44c0267b875
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-02-11 15:21:51 -0700

    merge

--- arrays/src/ThueMorse.java
+++ arrays/src/ThueMorse.java
@@ -1,27 +1,18 @@
 public class ThueMorse {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int r[] = new int[n];
-
-        for (int i = 0; i < n; i++) { // Using remarkable property from FAQ
-            if (i == 0) {
-                r[i] = 0;
-            } else if (i % 2 == 0) { // is even
-                r[i] = r[i / 2];
-            } else { // is odd
-                r[i] = 1 - r[i - 1];
-            }
-        }
+        int[] choices = {0, 1};
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (r[j] == r[i]) {
-                    StdOut.print("+  ");
-                } else StdOut.print("-  ");
+            boolean state = false;
 
+            int[] r = new int[n];
+            
+            for (int j = 0; j < n; j++) {
+                r[j] = choices[i];
             }
-            StdOut.println();
         }
+
 
     }
 }
