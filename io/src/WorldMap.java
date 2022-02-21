ad182f4bf677588671707e48f256de8926afd0f7
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-02-21 16:54:14 -0700

    no more random pen marks on WorldMap

--- io/src/WorldMap.java
+++ io/src/WorldMap.java
@@ -15,25 +15,37 @@ public class WorldMap {
                 int v = 0; // verts
                 for (int k = 0; k < 2; k++) { // read city and verts
                     String line_d = StdIn.readLine(); // read line
-                    if (k == 1) v = Integer.parseInt(line_d); // get verts
+                    if (k == 1 && (line_d != null)) v = Integer.parseInt(line_d); // get verts
+                    else StdOut.println(line_d);
+                    
                     if (v != 0) { // if verts != 0
                         double[] x = new double[v];
                         double[] y = new double[v];
                         for (int j = 0; j <= v; j++) {
                             String[] coordinate = StdIn.readLine().split(" ");
-                            for (int h = 0; h < coordinate.length; h++) {
+                            for (int h = 0, p = 0; h < coordinate.length; h++) {
                                 String s = coordinate[h];
                                 if (!s.isEmpty()) {
                                     double cord = Double.parseDouble(s);
-                                    if (h % 2 == 0) x[j] = cord;
-                                    else y[j] = cord;
+                                    if (p % 2 == 0) {
+                                        x[j] = cord;
+                                        StdOut.println("X: " + cord);
+                                    } else {
+                                        y[j] = cord;
+                                        StdOut.println("Y: " + cord);
+                                        StdOut.println();
+                                    }
+                                    p++;
+
                                 }
+
 
                             }
 
 
                         }
                         // Draw
+                        StdOut.println("X len: " + x.length + " Y len: " + y.length);
                         StdDraw.polygon(x, y);
                     }
                 }
