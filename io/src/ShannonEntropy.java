dd6d08fddcb4f7ccc4758ce20b67b459773f97bc
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-02-21 18:09:57 -0700

    Fixed and finished shanon

--- io/src/ShannonEntropy.java
+++ io/src/ShannonEntropy.java
@@ -1,24 +1,29 @@
 public class ShannonEntropy {
     public static void main(String[] args) {
         int[] buckets = new int[Integer.parseInt(args[0]) + 1];
-        int sum = Integer.parseInt(args[0]);
+        double sum = 0.0;
         double shanon = 0.0;
+        // Generate Buckets
         while (StdIn.hasNextLine()) { // Read next line as long as there's a next line: https://introcs.cs.princeton.edu/java/stdlib/javadoc/StdIn.html
             String[] line = StdIn.readLine().split(" ");
             for (String s : line) {
                 int n = Integer.parseInt(s);
                 buckets[n]++;
-                sum += n;
+                sum++;
             }
-            for (int s : buckets) {
-                double p = 1.0 * s / sum;
-                if (s != 0) {
-                    shanon += ((-1 * p) * Math.log(p) / Math.log(2));
-                }
 
-            }
 
         }
-        StdOut.println(shanon);
+        // Calculate Entrophy
+        for (int s : buckets) {
+            double p = s / sum;
+            if (s != 0) {
+                double sh = p * (Math.log(p) / Math.log(2));
+                shanon -= sh;
+            }
+        }
+
+        StdOut.printf("%.4f", shanon);
+        StdOut.println("\n");
     }
 }
