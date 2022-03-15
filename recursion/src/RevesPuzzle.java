5e935a485d58fb9617ebd84e3099195511dced9b
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-03-15 13:10:47 -0600

    reves

---
+++ recursion/src/RevesPuzzle.java
@@ -0,0 +1,22 @@
+public class RevesPuzzle {
+    // move n smallest discs from one pole to another, using the temp pole
+    private static void hanoi(long n, String from, String temp, String to) {
+        if (n == 0) return;
+        hanoi(n - 1, from, to, temp);
+        StdOut.println("Move disc " + n + " from " + from + " to " + to);
+        hanoi(n - 1, temp, from, to);
+    }
+
+    private static void reves(long n, String from, String temp1, String temp2, String to) {
+        long k = (Math.round((n + 1) - (Math.sqrt((2 * n) + 1))));
+        if (n == 0) return;
+        reves(k, from, temp2, to, temp1);
+        hanoi(n - k, from, temp2, to);
+        reves(k, temp1, temp2, from, to);
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        reves(n, "A", "C", "B", "D");
+    }
+}
