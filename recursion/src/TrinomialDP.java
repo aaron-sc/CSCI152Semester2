1e83a77a27447fa8cdbb588e62d5d90ffc47137b
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2022-03-16 10:49:32 -0600

    fix DP

--- recursion/src/TrinomialDP.java
+++ recursion/src/TrinomialDP.java
@@ -2,20 +2,18 @@ public class TrinomialDP {
     public static long trinomial(int n, int k) {
         if (n == 0 && k == 0) return 1; // base case
         if (k < -n || k > n) return 0; // base case
-        int[][] v = new int[n + 1][k + 1];
+        int[][] v = new int[2 * (n + 1)][2 * (n + 1)];
         v[0][0] = 1;
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                v[i][j] = v[n - 1][Math.abs(k - 1)] + v[n - 1][k] + v[n - 1][k + 1];
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    v[i][j] = v[i - 1][j] + (2 * v[i - 1][j + 1]);
+                } else {
+                    v[i][j] = v[i - 1][j - 1] + v[i - 1][j] + v[i - 1][j + 1];
+                }
             }
         }
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < k; j++) {
-//                StdOut.println(v[i][j]);
-//
-//            }
-//        }
+
         return v[n][k];
     }
 
